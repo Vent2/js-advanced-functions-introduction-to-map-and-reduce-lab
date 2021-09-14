804728118b3b000e884adbd325ff01ffb46903b3
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-09-14 02:13:14 -0400

    commit tree 60e2f078d5f06a694c5375f5ce695cd571390298

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function mapToNegativize(array) {
+    return array.map(
+        function(element) {
+            return element * -1;
+        }
+    )
+}
+
+function mapToNoChange(array) {
+    return array.map(
+        function(element) {
+            return element;
+        }
+    )
+}
+
+function mapToDouble(array) {
+    return array.map(
+        function(element) {
+            return element * 2;
+        }
+    )
+}
+
+function mapToSquare(array) {
+    return array.map(
+        function(element) {
+            return element * element;
+        }
+    )
+}
+
+
+function reduceToTotal(array, startingPoint= 0) {
+    return array.reduce(
+        function(accumulator, element) {
+            return accumulator + element;
+        },
+        startingPoint
+    )
+}
+
+function reduceToAllTrue(array) {
+    return array.every(item => !!item) ? true : false;
+}
+
+function reduceToAnyTrue(array) {
+    return array.some(item => !!item) ? true : false;
+}
